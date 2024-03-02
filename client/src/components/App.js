0109f015b8e2f1696527ae704159fbf0aa6f692c
Kevin <kevincastillo333@gmail.com>
2024-03-02 13:05:10 -0500

    App.py change - and testing creating new branch

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,30 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar'; 
+import Home from './components/Home';
+import AboutUs from './components/AboutUs';
+import MenuItems from './components/MenuItems';
+import ModifyReservation from './components/ModifyReservation'; 
+import OrderList from './components/OrderList'; 
+import ReservationForm from './components/ReservationForm'; 
+import './App.css'; 
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/menu" element={<MenuItems />} />
+          <Route path="/modify-reservation" element={<ModifyReservation />} />
+          <Route path="/order-list" element={<OrderList />} />
+          <Route path="/reservation-form" element={<ReservationForm />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
